4b01b2824a6d69583c30c4bed8b1e2f499dd1013
Leopoldo Avalos <leopoldo.avalos@bluesummit.de>
2021-03-01 15:41:38 +0100

    merge fixes

---
+++ wp-content/themes/bS/home.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+	Template Name: Home Template
+*/
+?>
+<h1>HOME TEMPLATE</h1>
+
+
+<?php
+/*
+echo do_shortcode( '[qsm quiz=' . intval( $_GET['quiz_id'] ) . ']' );
+*/
+?>
---
+++ wp-content/themes/bS/inc/template-custom_block.php
@@ -0,0 +1,21 @@
+<?php
+// Funktion Registrieren der Block Vorlage
+function my_block_template() {
+    register_block_pattern(
+        'my_template/example-block-template',
+        array('categories'  => array('buttons'),      
+            'title'     => 'Image | Text',
+            'content'   => "<!-- wp:columns {\"gradient\":\"luminous-vivid-amber-to-luminous-vivid-orange\"} -->\n<div class=\"wp-block-columns has-luminous-vivid-amber-to-luminous-vivid-orange-gradient-background has-background my_block\"><!-- wp:column {\"width\":\"50px\"} -->\n<div class=\"wp-block-column\" style=\"flex-basis:50px\"></div>\n<!-- /wp:column -->\n\n<!-- wp:column {\"width\":\"100%\"} -->\n<div class=\"wp-block-column\" style=\"flex-basis:100%\"></div>\n<!-- /wp:column --></div>\n<!-- /wp:columns -->",
+        )
+    );  
+    register_block_pattern(
+        'my_template/gallery',
+        array('categories'  => array('gallery'),      
+            'title'     => 'LB-Gallery',
+            'content'   => "<!-- wp:group {\"backgroundColor\":\"vivid-cyan-blue\"} -->\r\n<div class=\"wp-block-group has-vivid-cyan-blue-background-color has-background lb_gallery\"><div class=\"wp-block-group__inner-container\"><!-- wp:columns -->\r\n<div class=\"wp-block-columns\"><!-- wp:column -->\r\n<div class=\"wp-block-column lb_gallery_image\"></div>\r\n<!-- /wp:column -->\r\n<!-- wp:column {\"backgroundColor\":\"cyan-bluish-gray\"} -->\r\n<div class=\"wp-block-column lb_gallery_spacer has-cyan-bluish-gray-background-color has-background \"><!-- wp:spacer {\"height\":10} -->\r\n<div style=\"height:10px\" aria-hidden=\"true\" class=\"wp-block-spacer\"></div>\r\n<!-- /wp:spacer --></div>\r\n<!-- /wp:column -->\r\n<!-- wp:column -->\r\n<div class=\"wp-block-column lb_gallery_image\"></div>\r\n<!-- /wp:column --></div>\r\n<!-- /wp:columns --></div></div>\r\n<!-- wp:columns -->\r\n<div class=\"wp-block-columns\"><!-- wp:column {\"backgroundColor\":\"cyan-bluish-gray\"}  -->\r\n<div class=\"wp-block-column lb_gallery_spacer has-cyan-bluish-gray-background-color has-background\"><!-- wp:spacer {\"height\":10} -->\r\n<div style=\"height:10px\" aria-hidden=\"true\" class=\"wp-block-spacer\"></div>\r\n<!-- /wp:spacer --></div>\r\n<!-- /wp:column -->\r\n<!-- wp:column -->\r\n<div class=\"wp-block-column lb_gallery_image\"></div>\r\n<!-- /wp:column -->\r\n<!-- wp:column  {\"backgroundColor\":\"cyan-bluish-gray\"} -->\r\n<div class=\"wp-block-column lb_gallery_spacer has-cyan-bluish-gray-background-color has-background\"><!-- wp:spacer {\"height\":10} -->\r\n<div style=\"height:10px\" aria-hidden=\"true\" class=\"wp-block-spacer\"></div>\r\n<!-- /wp:spacer --></div>\r\n<!-- /wp:column --></div>\r\n<!-- /wp:columns -->\r\n<!-- /wp:group -->",
+        )
+    );
+}
+
+// Aufruf der Funktion im init Hook
+add_action( 'init', 'my_block_template' );
