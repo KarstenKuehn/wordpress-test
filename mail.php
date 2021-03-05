fc724167f14cfa063dcbc435af97363e98a1f517
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-03-05 13:56:48 +0100

    Automatic Push

--- mail.php
+++ mail.php
@@ -5,8 +5,8 @@ $post_ID = 4;
 $empfaenger = 'seo-cron@bluesummit.de';
 $betreff = 'Page wurde verändert '.$post_ID;
 $nachricht = 'no message';
-$header = 'From: wordpress@lottobayern.bluesummit.de' . "\r\n" .
-    'Reply-To: wordpress@lottobayern.bluesummit.de' . "\r\n" .
+$header = 'From: wordpress@bluesummit.de' . "\r\n" .
+    'Reply-To: wordpress@bluesummit.de' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($empfaenger, $betreff, $nachricht, $header);
