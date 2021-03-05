038c857dadaada7b47ee5b136a0a1ac4c7ea94f0
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-03-05 13:54:48 +0100

    slider

--- wp-content/themes/bS/index.php
+++ wp-content/themes/bS/index.php
@@ -104,6 +104,91 @@ get_header(); ?>
 	}
 	else
 	{
+?>
+
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 5</div>
+    <img src="http://lbup.local/wp-content/uploads/2021/02/lotto_small.png">
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 5</div>
+    <img src="http://lbup.local/wp-content/uploads/2021/02/lotto_transparent.png">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 5</div>
+    <img src="http://lbup.local/wp-content/uploads/2021/02/lotto_small.png">
+    <div class="text">Caption Three</div>
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">4 / 5</div>
+    <img src="http://lbup.local/wp-content/uploads/2021/02/lotto_transparent.png">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 5</div>
+    <img src="http://lbup.local/wp-content/uploads/2021/02/lotto_small.png">
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center" id="dots">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+var slides 		= document.getElementsByClassName("mySlides");
+var dots 		= document.getElementById("dots");
+var dots_html 	= '';
+dots.innerHTML 	= '';
+for (i = 1; i <= slides.length; i++) {
+	alert(i);
+	dots_html 	= '<span class="dot" onclick="currentSlide('+i+')"></span>';
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+<?php
 		//the_content();
 		echo get_my_content();
 	}
