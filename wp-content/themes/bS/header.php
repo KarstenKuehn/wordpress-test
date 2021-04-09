704e0188fa64c4d5a8946862347c138be11fc258
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-04-09 14:09:47 +0200

    Automatic Push

--- wp-content/themes/bS/header.php
+++ wp-content/themes/bS/header.php
@@ -1,14 +1,14 @@
 <?php seo_header();?>
 
 <body <?php body_class(); ?>>
-	<?php echo get_skiplinks();?>
-	<div class="lb_header">
-		<div class="lb_logo">
+	<?php echo get_skiplinks();?><div class="header">
 		<?php
 			// Site title or logo.
-			bs_site_logo();
+			//bs_site_logo();
 		?>
-		</div>
+		<a class="site-title" href="/">
+			<img class="logo" src="/wp-content/themes/bS/assets/logo.png"/>
+		</a>
 		<div class="lb_navi">
 			<?php
 			// Site Navi.
@@ -17,47 +17,23 @@
 			    'theme_location' => 'top-menu', 'depth' => 1 ) ); 					    
 			}						
 			?>
-			
 		</div>
-		<div class="lb_search" id="lb-search">
+		<div class="nav-frame">
 		<?php
 			// Site search
 			$enable_header_search = get_theme_mod( 'enable_header_search', true );
 						if ( true === $enable_header_search ) {
 							?>
-
-								<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-									<span class="toggle-inner">
-										<span class="toggle-icon">
-											<?php bs_the_theme_svg( 'search' ); ?>
-										</span>
-										<span class="toggle-text screen-reader-text"><?php _e( 'Suche', 'twentytwenty' ); ?></span>
-
-									</span>
-								</button><!-- .search-toggle -->
-
-							<?php
+			<img src="/wp-content/themes/bS/assets/search.svg" class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false"/>
+								<!-- .search-toggle -->
+		<?php
 						}
 		?>
+			<img src="/wp-content/themes/bS/assets/menu.svg" class="mobile_menu_button" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle"/>
+			<!-- .nav-toggle -->	
 		</div>
-		<div class="lb_menu">
-			<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
-				<span class="toggle-inner">
-					<span class="toggle-icon">
-						<?php bs_the_theme_svg( 'menu','ui','red' ); ?>
-					</span>
-					<span class="toggle-text screen-reader-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
-				</span>
-			</button><!-- .nav-toggle -->
-		</div>		
 	</div>
-	<?php
-	// Output the search modal (if it is activated in the customizer).
-	if ( true === $enable_header_search ) {
-		get_template_part( 'template-parts/modal-search' );
-	}
-	?>
+
 <?php seo_breadcrumb(); ?>
 <?php if (function_exists('nav_breadcrumb')) nav_breadcrumb(); 
-
 get_template_part( 'template-parts/modal-menu' );
