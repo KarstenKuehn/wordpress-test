d38e9ea9ba4fe76136daeddbfd074e87e5345305
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-04-28 17:38:31 +0200

    css image

--- wp-content/themes/bS/footer.php
+++ wp-content/themes/bS/footer.php
@@ -3,10 +3,10 @@
 <div class="footer1">
 <label class="footer_item1 label">Unsere Marken</label>
 <label class="footer_item1">
-<img width="53" height="56" src="http://lbup.local/wp-content/uploads/2021/04/lottoBayern.png" data-src="http://lbup.local/wp-content/uploads/2021/04/lottoBayern.png" alt="lottoBayern">	
+<img width="53" height="56" src="/wp-content/uploads/2021/04/lottoBayern.png" alt="lottoBayern">	
 </label>	
 <label class="footer_item1">
-<img width="109" height="56" src="http://lbup.local/wp-content/uploads/2021/04/Spielbanken_Bayern_Logo-1.png" data-src="http://lbup.local/wp-content/uploads/2021/04/Spielbanken_Bayern_Logo-1.png" alt="spielbanken">
+<img width="109" height="56" src="/wp-content/uploads/2021/04/Spielbanken_Bayern_Logo-1.png" alt="spielbanken">
 </label>
 <p>Spielteilnahme erst ab 18 Jahren. Glückspiel kann süchtig machen. Infos und Hilfe unter www.bzga.de.<br>
 Lotto Bayern verfüght über eine Erlaubnis der zuständigen Glückspielaufsichtsbehörde und steht unter deren Aufsicht.</p>
