e2d74dc5a5f6af0c386c936142e1a24456b357cf
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-05-03 17:31:07 +0200

    Automatic Push

--- wp-content/themes/bS/single.php
+++ wp-content/themes/bS/single.php
@@ -12,7 +12,7 @@ get_header();
 
 <div class="main">
 <?php
-		echo '<h1>'.get_the_title().'</h1>';
+		echo '<div class="height50"></div><h1>'.get_the_title().'</h1>';
 		$html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
 		echo $html;
 ?>
