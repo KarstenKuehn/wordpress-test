86d0031fb410ba9be761627a73e52060786def86
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-05-04 15:44:22 +0200

    Automatic Push

--- wp-content/themes/bS/events.php
+++ wp-content/themes/bS/events.php
@@ -38,18 +38,18 @@ echo '<div class="events_overview">
 <div class="events_header">';
 
 // -----------------------------------------------> YEAR FILTER
-echo '<div class="mobile_width25 desktop_width10"><select class="event_year">';
+echo '<div class="mobile_width25 desktop_width10"><select class="event_year" onchange="location = this.value;">';
 $i = 0;
 foreach ($years as $key => $year) 
 {
 	$i++;
 	if ($i == 1)
 	{
-		echo '<option selected>'.$year.'</option>';
+		echo '<option value="/presse-news/" selected>'.$year.'</option>';
 	}
 	else
 	{
-		echo '<option>'.$year.'</option>';
+		echo '<option value="/presse-news/?eyear='.$year.'">'.$year.'</option>';
 	}
 
 }
@@ -58,10 +58,25 @@ echo '</select></div>';
 // -----------------------------------------------> TOPIC FILTER
 
 echo '<div class="mobile_width25">
-<select class="event_topic">
-<option selected>Alle Themen</selected>
-<option>Termine</selected>
-<option>Veranstaltungen</selected>
+	<select class="event_topic" onchange="location = this.value;">
+	<option value="/presse-news/" ';
+	if(!isset($_GET['etype']) && !isset($_GET['eyear']))
+	{
+		echo 'selected';
+	}
+echo ' >Alle Themen</option>
+	<option value="/presse-news/?etype=termine" ';
+	if(isset($_GET['etype']) && $_GET['etype'] == 'termine')
+	{
+		echo 'selected';
+	}
+echo ' >Termine</option>
+	<option value="/presse-news/?etype=veranstaltungen"';
+	if(isset($_GET['etype']) && $_GET['etype'] == 'veranstaltungen')
+	{
+		echo 'selected';
+	}
+echo ' >Veranstaltungen</option>
 </select></div>';
 // -----------------------------------------------> TOPIC FILTER
 echo '<div class="mobile_width25 desktop_width40">&nbsp;</div>';
@@ -84,16 +99,48 @@ if (count($posts) == 1)
 echo '</span></div></div>';
 foreach ($posts as $key => $post) 
 {
+	if(!isset($_GET['etype']) && !isset($_GET['eyear']))
+	{
+		echo '<a class="event_short" href="'.$post->guid.'">';
+		echo '<span class="material-icons">east</span>';
+		echo '<h2>'.$post->post_title.'</h2>';
+		echo '<span>';
+		@the_field('datum');
+		echo '</span> | <span>';
+		@the_field('ort');
+		echo '</span> | <span>'.@get_the_category()[0]->cat_name.'</span';
+		echo '</a>';
+	}	
 
-	echo '<a class="event_short" href="'.$post->guid.'">';
-	echo '<span class="material-icons">east</span>';
-	echo '<h2>'.$post->post_title.'</h2>';
-	echo '<span>';
-	@the_field('datum');
-	echo '</span> | <span>';
-	@the_field('ort');
-	echo '</span> | <span>'.@get_the_category()[0]->cat_name.'</span';
-	echo '</a>';
+	if(	isset($_GET['eyear']) && $_GET['eyear'] == '20'.preg_replace('@\d\d\.\d\d.@','',@get_field('datum')) )
+	{
+		echo '<a class="event_short" href="'.$post->guid.'">';
+		echo '<span class="material-icons">east</span>';
+		echo '<h2>'.$post->post_title.'</h2>';
+		echo '<span>';
+		@the_field('datum');
+		echo '</span> | <span>';
+		@the_field('ort');
+		echo '</span> | <span>'.@get_the_category()[0]->cat_name.'</span';
+		echo '</a>';
+	}	
+
+	if(	isset($_GET['etype']) && $_GET['etype'] == strtolower(@get_the_category()[0]->cat_name) )
+	{
+		if ($_GET['etype'])
+		echo '<a class="event_short" href="'.$post->guid.'">';
+		echo '<span class="material-icons">east</span>';
+		echo '<h2>'.$post->post_title.'</h2>';
+		echo '<span>';
+		@the_field('datum');
+		echo '</span> | <span>';
+		@the_field('ort');
+		echo '</span> | <span>'.@get_the_category()[0]->cat_name.'</span';
+		echo '</a>';
+	}
+
+
+
 }
 echo '</div>';
 
