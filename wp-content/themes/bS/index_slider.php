1850222050f9695fc6a86bd261446049eff63a0a
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-05-04 16:33:28 +0200

    slider

--- wp-content/themes/bS/index_slider.php
+++ wp-content/themes/bS/index_slider.php
@@ -2,9 +2,177 @@
 /* 
 	Template Name: Index-Slider Template
 */
-get_header(); ?>
+get_header(); 
+$column_arr   = array('slide_img','slide_subline','slide_headline_zeile1','slide_headline_zeile2','slide_cta_btn_url' );
+$slide_count  = 0;
+
+/*
+  1. Mindestens 1 Slide definiert
+  2. Slide soll mindestens slide_img enthalten
+
+*/
+$slide_html='';
+for ($i=1; $i <= 3; $i++) 
+{ 
+  $field = get_field_object('slide_item'.$i);
+  echo '<b>'.$field['name'].'</b><br>';
+  $slide_ok = isset($field['value']['slide_img']) && !empty($field['value']['slide_img']);
+
+  if($slide_ok)
+  {
+    $slide_count++;
+
+
+    if( have_rows('slide_item'.$i) ):
+    while( have_rows('slide_item'.$i) ): the_row();
+      $slide_html.='<div class="mySlides">';
+        $slide_html.='<div class="my-block-image">';
+          $image = get_sub_field('slide_img');
+          $slide_html.='<img src="'.esc_url( $image['url'] ).'" alt="'.esc_attr( $image['alt'] ).'"/>';
+        $slide_html.='</div>';
+
+        $slide_html.='<div class="text">';
+
+          $slide_html.='
+          <span class="my-block-title">'.get_sub_field('slide_subline').'</span>
+          <span class="my-block-text">'.get_sub_field('slide_headline_zeile1').' </span>
+          <span class="my-block-text">'.get_sub_field('slide_headline_zeile2').'</span>';
+
+$link = get_sub_field('slide_cta_btn_url');
+if( $link ): 
+  print_r($link);
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+
+          $slide_html.='<a class="my-block-button" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.esc_html( $link_title ).'</a>';
+endif;
+        $slide_html.='</div>';
+      $slide_html.='</div>';
+    endwhile;
+
+    endif;
+
+  }
+
+}    
+  if($slide_count>0)
+  {
+    echo 'ausgeben<br>';
+    $slider='<div class="slideshow-container xx full">'.$slide_html.'</div>';
+
+
+
+
+
+  }
+echo $slider;
+?>
+
+
+<?php if( have_rows('slide_item1') ): ?>
+    <?php while( have_rows('slide_item1') ): the_row(); 
+
+        // Get sub field values.
+        $image = get_sub_field('slide_img');
+        $subline = get_sub_field('slide_subline');
+        ?>
+        <div id="hero">
+            <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
+            <div class="content">
+                <?php the_sub_field('slide_subline'); ?>
+            </div>
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
+<?php if( have_rows('slide_item2') ): ?>
+    <?php while( have_rows('slide_item2') ): the_row(); 
+
+        // Get sub field values.
+        $image = get_sub_field('slide_img');
+        $subline = get_sub_field('slide_subline');
+        ?>
+        <div id="hero">
+            <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
+            <div class="content">
+                <?php the_sub_field('slide_subline'); ?>
+            </div>
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
+<?php if( have_rows('slide_item3') ): ?>
+    <?php while( have_rows('slide_item3') ): the_row(); 
+
+        // Get sub field values.
+        $image = get_sub_field('slide_img');
+        $subline = get_sub_field('slide_subline');
+        ?>
+        <div id="hero">
+            <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
+            <div class="content">
+                <?php the_sub_field('slide_subline'); ?>
+            </div>
+        </div>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<div class="slideshow-container xx full">
+  <div class="mySlides">
+    <div class="my-block-image">
+      <img class="block_image" width="473" height="305" src="http://lbup.local/wp-content/uploads/2021/04/slide1.png" data-src="http://lbup.local/wp-content/uploads/2021/04/slide1.png" alt="slide1">
+    </div>
+    <div class="text">
+      <h3 id="headline_h3_0" class="my-block-title">NEWS &amp; PRESSE</h3>
+      <span class="my-block-text">Unser neues&nbsp;</span>
+      <span class="my-block-text">Spielangebot 2021 </span>
+      <a class="my-block-button" href="unternehmen">zum Artikel</a>
+    </div>
+  </div>
+  <div class="mySlides">
+    <div class="my-block-image">
+      <img class="block_image" width="473" height="305" src="http://lbup.local/wp-content/uploads/2021/04/slide2.png" data-src="http://lbup.local/wp-content/uploads/2021/04/slide2.png" alt="slide2">
+    </div>
+    <div class="text">
+      <h3 id="headline_h3_1" class="my-block-title">slide 2 subline</h3>
+      <span class="my-block-text">slide2 zeile 1 </span>
+      <span class="my-block-text">slide2 zeile2</span>
+      <a class="my-block-button" href="impressum">zum Artikel</a>
+    </div>
+  </div>
+  <div id="dots_0" class="dots 0">
+    <span class="dot active_dot" onclick="currentSlide(0,1)"></span>
+    <span class="dot" onclick="currentSlide(0,2)"></span>
+  </div>
+  <div id="slide_navi_0" class="slide_navi 0">
+    <a class="prev_slide" onclick="plusSlides(0,-1)">❮</a>
+    <a class="next_slide" onclick="plusSlides(0,+1)">❯</a>
+  </div>
+</div>
+
+
+
+
 <div class="main">
 <?php
+
+the_post();
+the_field('link1');
+echo '<img src="';
+the_field('image1');
+echo '" />';
+the_field('link2');
+$image = get_field('image2');
+echo '<img src="'. esc_url($image['url']).'" alt="'. esc_attr($image['alt']).'"/>';
+
+the_field('link3');
+$image = get_field('image3');
+echo '<img src="'. esc_url($image['url']).'" alt="'. esc_attr($image['alt']).'"/>';
+
+
+
+
 $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
 
 echo $html;
