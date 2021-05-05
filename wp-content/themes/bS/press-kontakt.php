71ab51e0752021625b5845dcb52b137060673cda
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-05-05 17:13:11 +0200

    Automatic Push

---
+++ wp-content/themes/bS/press-kontakt.php
@@ -0,0 +1,21 @@
+<?php
+/* 
+	Template Name: Presse Kontakt
+
+	This is the template for the single "Presse & News" articles
+*/
+
+get_header();
+
+?>
+
+
+
+
+<div class="main">
+<?php
+		
+		echo '<div class="height50"></div><h1>'.get_the_title().'</h1>';
+?>
+</div>
+<?php get_footer(); ?>
