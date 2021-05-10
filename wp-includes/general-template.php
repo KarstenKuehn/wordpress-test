a066ad2efdfb6d05559e374992cbb06805f81fdc
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-05-10 15:31:00 +0200

    general-template.php

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -333,7 +333,7 @@ function get_search_form( $args = array() ) {
 			$form = '<form role="search" ' . $aria_label . 'method="get" id="searchform" class="searchform" action="' . esc_url( home_url( '/' ) ) . '">
 				<div>
 					<label class="screen-reader-text" for="s">' . _x( 'Search for:', 'label' ) . '</label>
-					<input type="text" value="' . get_search_query() . '" name="s" id="s"  placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder' ) . '"/>
+					<input type="text" value="' . get_search_query() . '" name="s" id="s"/>
 					<input type="submit" id="searchsubmit" value="' . esc_attr_x( 'Search', 'submit button' ) . '" />
 				</div>
 			</form>';
