025d1d9d2e1b639afcd8e8b83743b4ee435da87b
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-05-17 15:08:43 +0200

    presse-kontakt-template

--- wp-content/themes/bS/press-kontakt.php
+++ wp-content/themes/bS/press-kontakt.php
@@ -14,9 +14,17 @@ get_header();
 
 <div class="main contact">
 <?php
+
 		
+
+
 		echo '<div class="height50"></div><h1>'.get_the_title().'</h1>';
 		echo '<section>';
+
+
+$html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
+echo $html;
+
 
 		echo @get_field('name').'<br>';
 		echo @get_field('e-mail').'<br>';
