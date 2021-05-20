3d9bc97a81c8a6801abcc79faf52957496460901
tatjana <tatjana.funkner@bluesummit.de>
2021-05-20 17:25:57 +0200

    hero-image-stairway

--- wp-content/themes/bS/single.php
+++ wp-content/themes/bS/single.php
@@ -8,13 +8,10 @@ get_header();
 
 <?php
 echo '<div class="main event_detail">';
-echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/></div>';
+echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
 echo '<h1>'.get_the_title().'</h1>';
-echo '<section>';
 $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
 echo $html;
-
-echo '</section>';
 
 $category = get_the_category()[0]->slug;
 
