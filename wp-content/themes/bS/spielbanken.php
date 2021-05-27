b644d92934299fa6838a3c352b8042f9856f1c0c
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-05-27 13:48:11 +0200

    Automatic Push

--- wp-content/themes/bS/spielbanken.php
+++ wp-content/themes/bS/spielbanken.php
@@ -9,7 +9,7 @@ get_header();
 <?php
 echo '<div class="main event_detail">';
 echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
-echo '<h1>'.get_the_title().'</h1>';
+#echo '<h1>'.get_the_title().'</h1>';
 $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
 echo $html;
 
