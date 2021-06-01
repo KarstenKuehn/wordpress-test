6a90a97702a2cc71f0c68ac6acd503090bdd3d19
Waldemar Schneider <waldemar.schneider@bluesummit.de>
2021-06-01 14:15:04 +0200

    Automatic Push

--- wp-content/themes/bS/single.php
+++ wp-content/themes/bS/single.php
@@ -5,9 +5,8 @@
 
 get_header();
 ?>
-<div class="main event_detail">
+<div class="main news_details">
 <?php
-echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
 echo '<h1>'.get_the_title().'</h1>';
 $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
 echo $html;
