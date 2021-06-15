38a3e27beb7b1f6137b588a68d6d121b37445afa
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-06-15 15:15:06 +0200

    kontakt-form

--- wp-content/themes/bS/standard.php
+++ wp-content/themes/bS/standard.php
@@ -25,7 +25,7 @@ get_header();
     </div>
 
 -->
-<div class="main event_detail">
+<div class="main standard">
 	<?php
 	echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
 	$html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
