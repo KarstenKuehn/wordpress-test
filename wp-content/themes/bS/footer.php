8df41070b7d8c5d162a1e9dad9df1a458feb106a
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-06-22 11:57:16 +0200

    footer.php logos ersetzen

--- wp-content/themes/bS/footer.php
+++ wp-content/themes/bS/footer.php
@@ -3,10 +3,10 @@
 <div class="footer1">
 <label class="footer_item1 label">Unsere Marken</label>
 <label class="footer_item1">
-<a href="https://www.lotto-bayern.de/" target="_blank"><img width="53" height="56" src="/wp-content/uploads/lottoBayern.png" alt="lottoBayern"></a>
+<a href="https://www.lotto-bayern.de/" target="_blank"><img width="53" height="56" src="/wp-content/uploads/LB_LOGO_pos_Z_18_01.png" alt="lottoBayern"></a>
 </label>	
 <label class="footer_item1">
-<a href="https://www.spielbanken-bayern.de/" target="_blank"><img width="109" height="56" src="/wp-content/uploads/SpielbankenBayern.png" alt="spielbanken"></a>
+<a href="https://www.spielbanken-bayern.de/" target="_blank"><img width="109" height="56" src="/wp-content/uploads/SPB-Logo_Schwarz.png" alt="spielbanken"></a>
 </label>
 <p>Spielteilnahme erst ab 18 Jahren. Glückspiel kann süchtig machen. Infos und Hilfe unter www.bzga.de.<br>
 Lotto Bayern verfügt über eine Erlaubnis der zuständigen Glückspielaufsichtsbehörde und steht unter deren Aufsicht.</p>
