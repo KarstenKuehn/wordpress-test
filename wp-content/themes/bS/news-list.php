dbf5ce613c28b686454e7618f119bda3dd123486
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-06-23 11:58:52 +0200

    news neue bilder

--- wp-content/themes/bS/news-list.php
+++ wp-content/themes/bS/news-list.php
@@ -38,9 +38,9 @@ foreach ($posts as $key => $post)
 	{
         $img='/wp-content/uploads/2021/06/SpielbankenBayern_allgemeines-PM-Motiv.png';
         if($sub_cat=='Unternehmen News')
-        $img='/wp-content/uploads/2021/06/Preview_Unternehmensnews_Teaser.jpeg';
+        $img='/wp-content/uploads/2021/06/Presse-Unternehmens-News_2000x1100.jpg';
         if($sub_cat=='Gewinner News')
-        $img='/wp-content/uploads/2021/06/Gewinnernews_Teaser.jpg';
+        $img='/wp-content/uploads/2021/06/Presse-News_Gewinnernews_2000x1100.jpg';
   		if (strlen(get_the_post_thumbnail_url()) > 0)
 		{
 			$img = get_the_post_thumbnail_url();
