12ca2765802dcd961c1007cea10f30242ebdbaac
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-06-29 17:44:10 +0200

    suche mit enter

--- wp-content/themes/bS/news-list.php
+++ wp-content/themes/bS/news-list.php
@@ -233,13 +233,13 @@ echo '<option'.$s2.' name="sort" value="asc">Nachrichten aufsteigend</option>';
 </div>
 </div>
 
-<div class="searchformfld" id="search-filter">
-            <input type="text" name="filter_word" value="<?php echo $filter_word ?>" id="filter_word" class="text-field" onClick="this.select()" placeholder=" "/>
-            <label for="filter_word">Suchbegriff eingeben</label>
-            <button onclick="searchStart()" class="mobile_hidden"><span class="material-icons">search</span></button>
-        </div>
-                    <button onclick="searchStart()" class="button desktop_hidden">Suche</button>
-</div>
+<form class="searchformfld" id="search-filter">
+      <input type="text" name="filter_word" value="<?php echo $filter_word ?>" id="filter_word" class="text-field" onClick="this.select()" placeholder=" "/>
+      <label for="filter_word">Suchbegriff eingeben</label>
+      <button onclick="searchStart()" class="mobile_hidden"><span class="material-icons">search</span></button>
+    </div>
+  <button onclick="searchStart()" class="button desktop_hidden">Suche</button>
+</form>
 
 <!-- spalten_3 || spalten_2 -->
 <div class="news spalten_3">
