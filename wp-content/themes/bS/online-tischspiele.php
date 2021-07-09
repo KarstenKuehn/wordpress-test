abd0f713de27c5e6e7e1739eaf0963eafa2ab24e
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-07-09 19:23:39 +0200

    slider tischspiele

--- wp-content/themes/bS/online-tischspiele.php
+++ wp-content/themes/bS/online-tischspiele.php
@@ -17,7 +17,37 @@ $args = array(
 );
 $pages = get_pages($args);
 	?>
-    <!-- Swiper -->
+
+    <div class="swiper-container mySwiper content-slider">
+      <div class="swiper-wrapper">
+		<?php
+			foreach ($pages as $page):
+				$url = get_the_post_thumbnail_url($page->ID);
+				echo '<div class="swiper-slide mySlides">';
+				echo '<div class="my-block-image" style="background-image:url(\''.$url.'\')";>';
+				echo '</div>';
+				echo '<div class="text cs">';
+				echo '<span class="my-block-text">';
+				echo $page->post_title;
+				echo '</span>';
+				echo '<span class="my-block-text">';
+				echo wp_trim_words(strip_tags($page->post_content),10, ' […]'  );   	
+				echo '</span>';
+				echo '<a class="button_more" href="' . get_permalink($page->ID) . '"><img class="icons" src="/wp-content/uploads/LB_Icons_export/cta_arrow.svg" alt="weiter" />';
+				echo '</a>';
+				echo '</div>';
+				echo '</div>';
+			endforeach;
+		?>
+      </div>
+      <div class="swiper-button-next aa"></div>
+      <div class="swiper-button-prev aa"></div>
+      <div class="swiper-pagination aa desktop_hidden"></div>
+    </div>
+
+
+
+    <!-- Swiper
     <div class="slideshow-container content-slider">
       <div class="swiper-wrapper">
 		<?php
@@ -42,14 +72,49 @@ $pages = get_pages($args);
       </div>
       <div class="swiper-button-next"></div>
       <div class="swiper-button-prev"></div>
-    </div>
+    </div> -->
 </div>
     <!-- Swiper JS -->
-	<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-	<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
     <!-- Initialize Swiper -->
     <script>
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 16,
+        slidesPerGroup: 1,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    // when window width is >= 640px
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 32
+    },
+      1110: {
+      slidesPerView: 3,
+      spaceBetween: 32
+    },},
+        pagination: {
+          el: ".swiper-pagination.aa",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next.aa",
+          prevEl: ".swiper-button-prev.aa",
+        },
+      });
+
+/*
+
+
       var swiper = new Swiper(".slideshow-container", {
 		slidesPerView: 3,
         spaceBetween: 30,   
@@ -63,12 +128,55 @@ $pages = get_pages($args);
           prevEl: ".swiper-button-prev",
         },
       });
+
+*/
+
     </script>
 
 
 
 
     <style type="text/css">
+
+
+
+
+
+      .swiper-container {
+        width: 100%;
+        height: 100%;
+        margin-bottom: 24px;
+      }
+
+      .swiper-slide {
+        text-align: center;
+        font-size: 18px;
+        background: #fff;
+
+        /* Center slide text vertically */
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
+        flex-direction: column;
+      }
+
+      .swiper-slide img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+
     	
       .swiper-container {
         width: 100%;
@@ -115,9 +223,16 @@ a.button_more img.icons {
 
 .my-block-image{
 height: 391px;
-width: 600px;
-object-fit: contain
+width: 100%;
+object-fit: contain;
+aspect-ratio:  600/491;
 }
+
+
+
+
+
+
 </style>
 
 <?php get_footer(); 
