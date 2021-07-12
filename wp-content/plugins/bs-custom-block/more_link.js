4143096706e364320fb2e5d72aa8fc5ad7785f13
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-07-12 16:40:24 +0200

    more_link.js

---
+++ wp-content/plugins/bs-custom-block/more_link.js
@@ -0,0 +1,140 @@
+(function (blocks, editor, components, i18n, element) {
+var el = element.createElement;
+var registerBlockType = blocks.registerBlockType;
+var RichText = editor.RichText;
+var BlockControls = editor.BlockControls;
+var AlignmentToolbar = editor.AlignmentToolbar;
+var MediaUpload = editor.MediaUpload;
+var InspectorControls = editor.InspectorControls;
+var TextControl = components.TextControl;
+
+  blocks.registerBlockType( 'lb/more-link', {
+    title: 'Link mit Pfeil', // The title of block in editor.
+    icon: 'admin-links', // The icon of block in editor.
+    category: 'common', // The category of block in editor.
+
+attributes: {
+  mediaID: {
+  type: 'number'
+  },
+  mediaURL: {
+  type: 'string',
+  source: 'attribute',
+  selector: 'img',
+  attribute: 'src'
+  },
+  mediaALT: {
+  type: 'string',
+  source: 'attribute',
+  selector: 'img',
+  attribute: 'alt'
+  },  
+  title: {
+  type: 'text',
+  selector: 'h3'
+  },
+  text: {
+  type: 'text',
+  selector: 'p'
+  },
+  buttonText: {
+  type: 'text'
+  },
+  buttonURL: {
+  type: 'url'
+  },
+  alignment: {
+  type: 'string',
+  default: 'center'
+  }
+},
+edit: function (props) {
+var attributes = props.attributes;
+var onSelectImage = function (media) {
+return props.setAttributes({
+mediaURL: media.url,
+mediaID: media.id,
+mediaALT: media.alt
+})
+};
+return [
+  el(
+    'div', {
+    className: '',
+    style: { textAlign: attributes.alignment,border:'1px solid grey',paddingBottom:'10px' }
+    },
+
+      el(
+        "hr",
+        null
+      ),
+      el(
+        "span",
+        null,
+        "Link URL: "
+      ),    
+      el(RichText, {
+      type: 'url',
+      label: i18n.__('Button URL', 'lb'),
+      value: attributes.buttonURL,    
+      key: 'editable',
+      tagName: 'p',
+      className: 'button-url',
+      placeholder: i18n.__('TextURL', 'lb'),
+      keepPlaceholderOnFocus: true,
+      value: attributes.buttonURL,
+      onChange: function (newURL) {
+      props.setAttributes({buttonURL: newURL})
+      }
+      }),
+      el(
+        "span",
+        null,
+        "Link Text: "
+      ),    
+      el(RichText, {
+      type: 'text',
+      label: i18n.__('Button Text', 'lb'),
+      value: attributes.buttonText,    
+      key: 'editable',
+      tagName: 'p',
+      className: 'button-text',
+      placeholder: i18n.__('ButtonText', 'lb'),
+      keepPlaceholderOnFocus: true,
+      value: attributes.buttonText,
+      onChange: function (newbuttonText) {
+      props.setAttributes({buttonText: newbuttonText})
+      }
+      }),  
+  )
+
+];
+},
+save: function (props) {
+var attributes = props.attributes;
+
+
+return (
+  el('section', {className: 'content_section',},
+  el('a', {
+    className: 'more-link',
+    href: attributes.buttonURL
+    },   
+    attributes.buttonText,
+    el('span',{className: 'material-icons',},'east')  
+  )
+  )
+
+)// end return
+}
+})
+})(
+window.wp.blocks,
+window.wp.editor,
+window.wp.components,
+window.wp.i18n,
+window.wp.element
+);
+
+
+
