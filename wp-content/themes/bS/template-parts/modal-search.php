387362b19c4caae1ca2b997eb127519e7a26a0a8
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-07-21 11:18:57 +0200

    suche schließen

--- wp-content/themes/bS/template-parts/modal-search.php
+++ wp-content/themes/bS/template-parts/modal-search.php
@@ -36,7 +36,7 @@
 			?>
 
 			<button class="toggle search-untoggle close-search-toggle fill-children-current-color" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-				<span class="screen-reader-text"><?php _e( 'Close search', 'bS' ); ?></span><?php echo bs_get_theme_svg( 'cross','ui','#1F5DA6' ); ?>
+				<span class="screen-reader-text"><?php _e( 'Suche schließen', 'bS' ); ?></span><?php echo bs_get_theme_svg( 'cross','ui','#1F5DA6' ); ?>
 			</button><!-- .search-toggle -->
 
 		</div><!-- .section-inner -->
