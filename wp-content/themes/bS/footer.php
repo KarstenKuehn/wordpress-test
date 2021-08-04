227f66e1cb6403de42b84a65c8c999066db55c7e
Karsten Kühn <karsten.kuehn@bluesummit.de>
2021-08-04 15:44:44 +0200

    list elemente ul > li

--- wp-content/themes/bS/footer.php
+++ wp-content/themes/bS/footer.php
@@ -1,23 +1,23 @@
-<?php //wp_footer();?>
 <footer>
-<div class="footer">
-<div class="footer1">
-<h3 class="footer_item1 label" tabindex="0">Unsere Marken</h3>
-<label class="footer_item1">
-<a href="https://www.lotto-bayern.de/" target="_blank" rel="noreferrer"><img width="53" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/LB_LOGO.svg" alt="zu LOTTO Bayern"></a>
-</label>	
-<label class="footer_item1">
-<a href="https://www.spielbanken-bayern.de/" target="_blank" rel="noreferrer"><img width="109" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/SPB-Logo.svg" alt="zu Spielbanken Bayern"></a>
-</label>
-<p>Spielteilnahme erst ab 18 Jahren und in den Bayrischen Spielbanken ab 21 Jahren. Glücksspiel kann süchtig machen.<br />
-    Infos und Hilfe unter <a style="border:none" href="https://www.bzga.de/" target="_blank">www.bzga.de</a>.<br />
-Die Staatliche Lotterie- und Spielbankverwaltung Bayern verfügt über eine Erlaubnis der zuständigen Glücksspielaufsichtsbehörden und steht unter deren Aufsicht.</p>
-<p>Alle Angaben ohne Gewähr.</p>
-</div><nav class="footer-liste">
-<?php
-    echo get_FooterMenu();
-?>	
-</nav>
-</div>
+    <div class="footer">
+        <div class="footer1">
+            <ul>
+                <li class="footer_item1" tabindex="0">
+                    <span class="label">Unsere Marken</span>
+                </li>
+                <li class="footer_item1" tabindex="0">
+                    <a href="https://www.lotto-bayern.de/" target="_blank" rel="noreferrer"><img width="53" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/LB_LOGO.svg" alt="zu LOTTO Bayern"></a>
+                </li>
+                <li class="footer_item1" tabindex="0">
+                    <a href="https://www.spielbanken-bayern.de/" target="_blank" rel="noreferrer"><img width="109" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/SPB-Logo.svg" alt="zu Spielbanken Bayern"></a>
+                </li>
+            </ul>
+            <p>Spielteilnahme erst ab 18 Jahren und in den Bayrischen Spielbanken ab 21 Jahren. Glücksspiel kann süchtig machen.<br />
+                Infos und Hilfe unter <a style="border:none" href="https://www.bzga.de/" target="_blank">www.bzga.de</a>.<br />
+            Die Staatliche Lotterie- und Spielbankverwaltung Bayern verfügt über eine Erlaubnis der zuständigen Glücksspielaufsichtsbehörden und steht unter deren Aufsicht.</p>
+            <p>Alle Angaben ohne Gewähr.</p>
+        </div>
+        <nav class="footer-liste"><?php echo get_FooterMenu(); ?></nav>
+    </div>
 </footer>
 <?php footer();?>
