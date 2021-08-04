f45b3f0a9cf4aed43c1d52179f19bed100f78015
Karsten Kühn <karsten.kuehn@bluesummit.de>
2021-08-04 15:53:18 +0200

    Ticket LOBYWR-305 main tag

--- wp-content/themes/bS/index_slider.php
+++ wp-content/themes/bS/index_slider.php
@@ -2,20 +2,15 @@
 /* 
 	Template Name: Index-Slider Template
 */
-get_header(); 
-?>
-
-<div class="main">
-<?php
+get_header();
 
 $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
-
-echo $html;
-
-  //echo get_my_content();
 ?>
-</div>
-
+    <main>
+        <div class="main">
+            <?php echo $html; ?>
+        </div>
+    </main>
 <?php get_footer(); ?>
 
     <!-- Initialize Swiper -->
