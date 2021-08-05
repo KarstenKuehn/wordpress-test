f478c3629b9b587609dd84e9926ee0af705d23a8
Karsten Kühn <karsten.kuehn@bluesummit.de>
2021-08-05 11:31:24 +0200

    Ticket LOBYWR-404: Skiplinks (BITV)

--- wp-content/themes/bS/footer.php
+++ wp-content/themes/bS/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer id="footer">
     <div class="footer">
         <div class="footer1">
             <ul>
@@ -6,18 +6,20 @@
                     <span class="label">Unsere Marken</span>
                 </li>
                 <li class="footer_item1" tabindex="0">
-                    <a href="https://www.lotto-bayern.de/" target="_blank" rel="noreferrer"><img width="53" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/LB_LOGO.svg" alt="zu LOTTO Bayern"></a>
+                    <a href="https://www.lotto-bayern.de/" target="_blank" rel="noreferrer"><img width="53" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/LB_LOGO.svg" alt="zur Webseite von LOTTO Bayern"></a>
                 </li>
                 <li class="footer_item1" tabindex="0">
-                    <a href="https://www.spielbanken-bayern.de/" target="_blank" rel="noreferrer"><img width="109" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/SPB-Logo.svg" alt="zu Spielbanken Bayern"></a>
+                    <a href="https://www.spielbanken-bayern.de/" target="_blank" rel="noreferrer"><img width="109" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/SPB-Logo.svg" alt="zur Webseite von Spielbanken Bayern"></a>
                 </li>
             </ul>
             <p>Spielteilnahme erst ab 18 Jahren und in den Bayrischen Spielbanken ab 21 Jahren. Glücksspiel kann süchtig machen.<br />
-                Infos und Hilfe unter <a style="border:none" href="https://www.bzga.de/" target="_blank">www.bzga.de</a>.<br />
+                Infos und Hilfe unter <a href="https://www.bzga.de/" target="_blank">www.bzga.de</a>.<br />
             Die Staatliche Lotterie- und Spielbankverwaltung Bayern verfügt über eine Erlaubnis der zuständigen Glücksspielaufsichtsbehörden und steht unter deren Aufsicht.</p>
             <p>Alle Angaben ohne Gewähr.</p>
         </div>
+        <a id="footer-navigation" aria-label="Informationen zum Webseiten-Betreiber"></a>
         <nav class="footer-liste"><?php echo get_FooterMenu(); ?></nav>
     </div>
+    <a href="#top" aria-label="zum Anfang der Seite springen"></a>
 </footer>
 <?php footer();?>
