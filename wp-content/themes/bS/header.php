9210dc83c3e971cbb480b9a23789541d7aee19ac
Karsten Kühn <karsten.kuehn@bluesummit.de>
2021-08-06 09:57:15 +0200

    Ticket LOBYWR-402: Close Button

--- wp-content/themes/bS/header.php
+++ wp-content/themes/bS/header.php
@@ -33,7 +33,7 @@
     </div>
     <div class="sub_menu mobile_hidden" id="sub_menu">
         <div class="sub_menu_inner">
-        <div class="mobile_hidden menu_close"><button onclick="closeNavi()"><span class="material-icons">close</span></button></div>
+        <div class="mobile_hidden menu_close"><button tabindex="0" onclick="closeNavi()"><span class="material-icons">close</span></button></div>
         <?php echo getSubMenu($name_of_menu,'d'); ?>
         </div>
     </div>
