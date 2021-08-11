c7206564fe4363c62a86244c56d6391d297db57c
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-08-11 18:56:29 +0200

    text-image

--- wp-content/plugins/bs-custom-block/bitv-image-text.js
+++ wp-content/plugins/bs-custom-block/bitv-image-text.js
@@ -1,12 +1,12 @@
 (function (blocks, editor, components, i18n, element) {
   var el = element.createElement;
 const iconEl = el('svg', { width: 20, height: 20 },
-  el('path', { d: "M22 13h-8v-2h8v2zm0-6h-8v2h8V7zm-8 10h8v-2h-8v2zm-2-8v6c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V9c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2zm-1.5 6l-2.25-3-1.75 2.26-1.25-1.51L3.5 15h7z" } )
+  el('path', { d: "M14,17H7v-2h7V17z M17,13H7v-2h10V13z M17,9H7V7h10V9z" } )
 );
   var InnerBlocks = wp.editor.InnerBlocks;
   blocks.registerBlockType( 'bitv/image-text', {
     title: 'BITV-Image-Text', // The title of block in editor.
-    icon: iconEl, // The icon of block in editor.
+    icon: 'align-pull-left', // The icon of block in editor.
     category: 'bitv-blocks', // The category of block in editor.
     attributes: {
       headline: {
