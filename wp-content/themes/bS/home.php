8eaf55fd2c9e9436bb64f976a45bf6bf402e0edf
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-08-18 14:22:12 +0200

    teaser standard

--- wp-content/themes/bS/home.php
+++ wp-content/themes/bS/home.php
@@ -4,7 +4,7 @@ get_header();
 <main>
     <div class="main">
     <?php
-            echo '<h1>'.get_the_title().'</h1>';
+            //echo '<h1>'.get_the_title().'</h1>';
             $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
             echo $html;
     ?>
