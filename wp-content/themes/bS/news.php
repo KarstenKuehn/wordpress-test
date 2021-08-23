127a47ebcdee5d2342ff2b9e5cbe49c8b62549f5
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-08-23 11:11:32 +0200

    news.php

--- wp-content/themes/bS/news.php
+++ wp-content/themes/bS/news.php
@@ -10,9 +10,8 @@ get_header();
     <?php
 
     echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
-    echo '<div class="height50"></div>
-    <h1>Aktuelle Pressemitteilungen und News</h1>';
-
+    //echo '<div class="height50"></div>';
+    echo '<h1>Aktuelle Pressemitteilungen und News</h1>';
     $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
     echo $html;
 
