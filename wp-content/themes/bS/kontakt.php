24e29743b4591894e718b6293b40754688207c14
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-08-23 13:14:47 +0200

    kontakt

--- wp-content/themes/bS/kontakt.php
+++ wp-content/themes/bS/kontakt.php
@@ -7,7 +7,7 @@ get_header();
 <main id="maincontent">
     <div class="main">
         <?php
-        echo '<h1 tabindex="0">'.get_the_title().'</h1>';
+        //echo '<h1 tabindex="0">'.get_the_title().'</h1>';
         $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
         echo $html;
         ?>
