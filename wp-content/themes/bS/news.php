5da490522aad09fba92f871c2db6169a016e60a0
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-09-08 15:13:30 +0200

    header mit stufe

--- wp-content/themes/bS/news.php
+++ wp-content/themes/bS/news.php
@@ -9,7 +9,7 @@ get_header();
     <div class="main news_feed">
     <?php
 
-    echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
+    //echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
     
   //  echo '<section class="content_section bitv"><div class="modul"><h1 class="news_headline e_headline has-large-font-size">Aktuelle Pressemitteilungen und News</h1></div>';
 
