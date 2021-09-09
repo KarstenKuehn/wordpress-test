939d584bc76f87ff8400fb40169991a6f3e2bb1b
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-09-09 11:25:03 +0200

    style: sup, sub, italic

--- wp-content/themes/bS/lotterien.php
+++ wp-content/themes/bS/lotterien.php
@@ -8,7 +8,7 @@ get_header();
 
 <div class="main standard">
 	<?php
-	echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
+	//echo '<div class="bg-image" style="background-image:url(\''.get_the_post_thumbnail_url().'\');"/><div class="hero-image-stairway"></div></div>';
 	$html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
 	echo $html;
   ?>
