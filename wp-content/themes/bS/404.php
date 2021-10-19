e04ce9df9a7aa1ee7a701271ac0ce41c2bafb3ed
Karsten Kühn <karsten.kuehn@bluesummit.de>
2021-10-19 13:40:58 +0200

    Ticket LOBYWR-104 404-Seite Bugfix div id maincontent

--- wp-content/themes/bS/404.php
+++ wp-content/themes/bS/404.php
@@ -4,15 +4,12 @@ Template Name: 404 Template
 */
 get_header();
 ?>
-<?php
-//wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
-?>
-    <main>
-        <div class="main">
-            <?php
+<main id="maincontent">
+    <div class="main">
+        <?php
             $html = preg_replace('/(\>)\s*(\<)/m', '$1$2', get_my_content());
             echo $html;
-            ?>
+        ?>
         </div>
     </main>
 <?php
