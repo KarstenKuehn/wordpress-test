7a482e6aba40271af6c59c6735b4db3d42a996c5
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-10-20 11:20:03 +0200

    Footertext Änderung => LOBYWR-450

--- wp-content/themes/bS/footer.php
+++ wp-content/themes/bS/footer.php
@@ -9,10 +9,9 @@
                     <a href="https://www.spielbanken-bayern.de/" target="_blank" rel="noreferrer"><img width="109" height="56" src="https://lotterien-spielbanken-bayern.de/wp-content/uploads/2021/07/SPB-Logo.svg" alt="Logo Spielbanken Bayern" title="zur Webseite von Spielbanken Bayern"></a>
                 </li>
             </ul>
-            <p>Spielteilnahme erst ab 18 Jahren und in den Bayrischen Spielbanken ab 21 Jahren. Glücksspiel kann süchtig machen.<br />
-                Infos und Hilfe unter <a href="https://www.bzga.de/" target="_blank">www.bzga.de</a>.<br />
-            Die Staatliche Lotterie- und Spielbankverwaltung Bayern verfügt über eine Erlaubnis der zuständigen Glücksspielaufsichtsbehörden und steht unter deren Aufsicht.</p>
-            <p>Alle Angaben ohne Gewähr.</p>
+            <p>Die Staatliche Lotterie- und Spielbankverwaltung verfügt über eine Erlaubnis der zuständigen Glücksspielaufsichtsbehörden und steht unter deren Aufsicht.</p>
+            <p>Spielteilnahme erst ab 18 Jahren und in den Bayerischen Spielbanken ab 21 Jahren.</p>
+            <p>Glücksspiel kann süchtig machen. Infos und Hilfe unter <a href="https://www.bzga.de/" target="_blank" title="gehe zu Bundeszentrale für gesundheitliche Aufklärung">www.bzga.de</a>.</p>
         </div>
         <a id="footer-navigation" aria-label="Informationen zum Webseiten-Betreiber"></a>
         <nav class="footer-liste"><?php echo get_FooterMenu(); ?></nav>
