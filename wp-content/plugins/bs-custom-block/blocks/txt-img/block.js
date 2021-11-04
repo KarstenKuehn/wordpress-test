308c1e372c8c34ece7c40ef4815b980eceb0c5f1
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-11-04 12:49:30 +0100

    txt-img rollback

--- wp-content/plugins/bs-custom-block/blocks/txt-img/block.js
+++ wp-content/plugins/bs-custom-block/blocks/txt-img/block.js
@@ -2,33 +2,17 @@
   var el = element.createElement;
 
   var InnerBlocks = wp.editor.InnerBlocks;
-
-blocks.registerBlockStyle( 'core/column', {
-name: 'spiegeln',
-label: 'Spiegeln'
-} );
-
   blocks.registerBlockType( 'bitv/text-image', {
     title: 'Text-Bild Modul', // The title of block in editor.
    icon: 'align-pull-right', // The icon of block in editor.
     category: 'bitv-blocks', // The category of block in editor.  
     edit: function() {
-      return el( 'div', { className:'block-frame',style: { outline: '1px solid gray', padding: 5 } },
+      return el( 'div', { style: { outline: '1px solid gray', padding: 5 } },
         el( 'span', {}, 'Text-Bild-Modul' ),
         el(
           InnerBlocks,
           {
             template: [
-                  ['core/column',{className:'block-text content',},
-                    [
-                      ['core/heading',{className:'e_headline content modul-title','placeholder':'Modul Überschrift',fontSize: 'large'}],
-                      ['core/image',{className:'modul-bild','placeholder':'Modul-Bild'}],
-                      ['core/paragraph',{className:'modul-text','placeholder':'Modul-Text'}],
-                      ['core/button',{className:'modul-button content','placeholder':'Modul-Button'}],
-                      
-                    ]
-                  ],
-/*
               ['core/heading',{className:'e_headline content','placeholder':'Block Überschrift',fontSize: 'large'}],
 
                   ['core/column',{className:'block-text content',},
@@ -39,8 +23,6 @@ label: 'Spiegeln'
                   ],
               ['core/button',{className:'modul-button content','placeholder':'Button'}],
               ['core/image',{className:'modul-image','placeholder':'Verlinkung-Bild'}],
-
-*/              
             ],
             //templateLock: "all",
             //allowedBlocks: ['core/columns'],
@@ -50,7 +32,7 @@ label: 'Spiegeln'
     },
 
     save: function() {
-      return el( 'div', { className:'modul text-bild' },
+      return el( 'div', { className:'modul text-image' },
         el( InnerBlocks.Content, {} )
       );
     },
