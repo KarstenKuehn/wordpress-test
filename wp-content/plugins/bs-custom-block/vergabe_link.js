7c0789abd028b0babfbf8a74b79a7c147f9b1c9e
Tatjana Funkner <tatjana.funkner@bluesummit.de>
2021-11-08 18:31:01 +0100

    LOBYWR-452 => Vergabedokumente mit Ablaufdatum

---
+++ wp-content/plugins/bs-custom-block/vergabe_link.js
@@ -0,0 +1,226 @@
+(function (blocks, editor, components, i18n, element) {
+var el = element.createElement;
+var registerBlockType = blocks.registerBlockType;
+var RichText = editor.RichText;
+var BlockControls = editor.BlockControls;
+var AlignmentToolbar = editor.AlignmentToolbar;
+var MediaUpload = editor.MediaUpload;
+var InspectorControls = editor.InspectorControls;
+var TextControl = components.TextControl;
+
+  var RangeControl = wp.components.RangeControl;
+
+var DateTimePicker = wp.components.DateTimePicker;
+
+var d = new Date();
+
+var tag = d.getDate();
+
+var monat = d.getMonth() + 1;
+
+var jahr = d.getFullYear();
+
+var uhrzeit = tag + "." + monat + "." + jahr ;
+ var __ = wp.i18n.__;
+  blocks.registerBlockType( 'lb/vergabe-link', {
+    title: 'Vergabe-Link', // The title of block in editor.
+    icon: 'admin-links', // The icon of block in editor.
+    category: 'common', // The category of block in editor.
+
+attributes: {
+  mediaID: {
+  type: 'number'
+  },
+  mediaURL: {
+  type: 'string',
+  source: 'attribute',
+  selector: 'img',
+  attribute: 'src'
+  },
+  mediaALT: {
+  type: 'string',
+  source: 'attribute',
+  selector: 'img',
+  attribute: 'alt'
+  },  
+  title: {
+  type: 'text',
+  selector: 'h3'
+  },
+  text: {
+  type: 'text',
+  selector: 'p'
+  },
+  buttonText: {
+  type: 'text'
+  },
+  buttonURL: {
+  type: 'url'
+  },
+  alignment: {
+  type: 'string',
+  default: 'center'
+  },
+    ablaufdatum: {
+      type: 'string', 
+      default: uhrzeit
+    },
+
+        dateFrom: {
+            type: 'strings',
+        },
+        dateTo: {
+            type: 'string',
+        },
+
+
+
+},
+edit: function (props) {
+var attributes = props.attributes;
+var onSelectImage = function (media) {
+return props.setAttributes({
+mediaURL: media.url,
+mediaID: media.id,
+mediaALT: media.alt
+})
+};
+return [
+  el(
+    'div', {
+    className: '',
+    style: { textAlign: attributes.alignment,border:'1px solid grey',paddingBottom:'10px' }
+    },
+    el(InspectorControls, {
+      key: "setting"
+    },
+
+/*
+
+      el("div", {
+        id: "Ablaufdatum Controls"
+      },
+        el("fieldset", null, el("legend", {
+          className: "blocks-base-control__label"
+        }, (0, __)('Ablaufdatum', 'gutenpride')), el(TextControl, {
+          value: props.attributes.ablaufdatum,
+          onChange: (value) => {
+            props.setAttributes({ ablaufdatum: value });
+          }
+        }))
+
+      )
+,
+  
+*/
+      el("div", {
+        id: "test"
+      },
+        el("fieldset", null, el("legend", {
+          className: "blocks-base-control__label"
+        }, (0, __)('Ablaufdatum test', 'gutenpride')), el(DateTimePicker, {
+          label:'myLabel',
+          value: props.attributes.dateTo,
+          currentDate:props.attributes.dateTo,
+          onChange: (value) => {
+            props.setAttributes({ dateTo: value });
+          }
+        }))
+
+      )
+
+
+      ),
+
+
+
+      el(
+        "hr",
+        null
+      ),
+      el(
+        "span",
+        null,
+        "Link URL: "
+      ),    
+      el(RichText, {
+      type: 'url',
+      label: i18n.__('Button URL', 'lb'),
+      value: attributes.buttonURL,    
+      key: 'editable',
+      tagName: 'p',
+      className: 'button-url',
+      placeholder: i18n.__('TextURL', 'lb'),
+      keepPlaceholderOnFocus: true,
+      value: attributes.buttonURL,
+      onChange: function (newURL) {
+      props.setAttributes({buttonURL: newURL})
+      }
+      }),
+      el(
+        "span",
+        null,
+        "Link Text: "
+      ),    
+      el(RichText, {
+      type: 'text',
+      label: i18n.__('Button Text', 'lb'),
+      value: attributes.buttonText,    
+      key: 'editable',
+      tagName: 'p',
+      className: 'button-text',
+      placeholder: i18n.__('ButtonText', 'lb'),
+      keepPlaceholderOnFocus: true,
+      value: attributes.buttonText,
+      onChange: function (newbuttonText) {
+      props.setAttributes({buttonText: newbuttonText})
+      }
+      }),  
+  )
+
+];
+},
+save: function (props) {
+var attributes = props.attributes;
+datei_ablaufdatum = attributes.dateTo;
+testdatum = uhrzeit;
+testdatum = '2021-11-08T17:23:00';
+return (
+//  attributes.ablaufdatum == '8.11.2021'?
+
+   Date.parse(datei_ablaufdatum) >= Date.parse(testdatum)?
+
+    el('a', {
+      className: 'vergabe-link',
+      href: attributes.buttonURL,
+      //ablaufdatum:attributes.ablaufdatum,
+      dateTo:datei_ablaufdatum,
+      },   
+      attributes.buttonText,
+      el('span',{className: 'material-icons',},'east')  
+    )
+:
+
+    el('a', {
+      className: 'vergabe-link',
+      href: attributes.buttonURL,
+      //ablaufdatum:attributes.ablaufdatum,
+      dateTo:datei_ablaufdatum,
+      },   
+      attributes.buttonText,
+      el('span',{className: 'material-icons',},'west')  
+    )
+)// end return
+}// end save()
+}//end registerBlockType
+)
+})(
+window.wp.blocks,
+window.wp.editor,
+window.wp.components,
+window.wp.i18n,
+window.wp.element
+);
+
+
+
