92e4c859cb807520a6b459f70b7e2fbccd1bc0b3
Karsten Kühn <karsten.kuehn@bluesummit.de>
2021-11-08 13:46:23 +0100

    Ticket LOBYWR-470 BITV Nachtest Navigation: Bugfixes accessibleMegaMenu

--- wp-content/themes/bS/assets/js/bs_main_nav_walker.js
+++ wp-content/themes/bS/assets/js/bs_main_nav_walker.js
@@ -20,6 +20,34 @@ if (jQuery) {
             }, 500);
         });
 
+        <!-- initialize a selector as an accessibleMegaMenu -->
+        $("#navigation").accessibleMegaMenu({
+            /* prefix for generated unique id attributes, which are required
+           to indicate aria-owns, aria-controls and aria-labelledby */
+            uuidPrefix: "accessible-megamenu",
+
+            /* css class used to define the megamenu styling */
+            menuClass: "nav-menu",
+
+            /* css class for a top-level navigation item in the megamenu */
+            topNavItemClass: "nav-item",
+
+            /* css class for a megamenu panel */
+            panelClass: "sub-nav",
+
+            /* css class for a group of items within a megamenu panel */
+            panelGroupClass: "sub-nav-group",
+
+            /* css class for the hover state */
+            hoverClass: "hover",
+
+            /* css class for the focus state */
+            focusClass: "focus",
+
+            /* css class for the open state */
+            openClass: "active"
+        });
+
         function closeMenu() {
             $( ".nav-container__main-nav li.nav-item > .main-nav_submenu" )
                 .each( function( index, $el ) {
